47ce0408d5761543a925084f9fa24552868fa332
Gary Talent <gtalent2@gmail.com>
2019-06-15 12:47:11 -0500

    [ox/std] Add substitutes file with missing __cxa_pure_virtual handler

---
+++ deps/ox/src/ox/std/substitutes.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 - 2018 gtalent2@gmail.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef OX_USE_STDLIB
+
+#include <cstdlib>
+#include <stdio.h>
+
+// weak link panic to ensure that it exists, but allow it to be overriden by
+// platform specific code
+void __attribute__((weak)) panic(const char *msg) {
+	printf("Unexpected state:\n%s", msg);
+	std::abort();
+}
+
+extern "C" {
+	
+void __cxa_pure_virtual() { 
+	panic("Missing virtual function");
+}
+
+}
+
+#endif

d4212abe3b2b94d8a100a1908853a97de6e561c0
Gary Talent <gtalent2@gmail.com>
2019-07-07 22:08:39 -0500

    [ox/fs] Fix PassthroughFS to pull in defines from header before check

--- deps/ox/src/ox/fs/filesystem/passthroughfs.cpp
+++ deps/ox/src/ox/fs/filesystem/passthroughfs.cpp
@@ -6,11 +6,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#if OX_PASSTHROUGHFS_HAS_DEPENDENCIES && defined(OX_USE_STDLIB)
+#include "passthroughfs.hpp"
+
+#if defined(OX_PASSTHROUGHFS_HAS_DEPENDENCIES)
 
 #include <stdio.h>
-
-#include "passthroughfs.hpp"
 
 namespace ox {
 

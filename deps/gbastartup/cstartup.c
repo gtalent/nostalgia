2f45cbe6d94bf50d6d28adc7bc02ac7b0351e509
Gary Talent <gtalent2@gmail.com>
2019-11-09 00:36:26 -0600

    [gbastartup] Cleanup

--- deps/gbastartup/cstartup.c
+++
@@ -1,9 +0,0 @@
-
-extern void (*__preinit_array_start[]) (void);
-extern void (*__preinit_array_end[]) (void);
-extern void (*__init_array_start[]) (void);
-extern void (*__init_array_end[]) (void);
-
-extern "C" void __libc_init_array() {
-	auto count = __preinit_array_end - __preinit_array_start;
-}

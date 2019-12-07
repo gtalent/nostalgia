c6ecbd6d125ae28e7d9fbcd9f148445a1f1d3afd
Gary Talent <gtalent2@gmail.com>
2019-12-06 23:46:12 -0600

    [nostalgia/studio] Actually add studio::Editor...

---
+++ src/nostalgia/studio/lib/editor.hpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 - 2019 gtalent2@gmail.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <QUndoStack>
+#include <QWidget>
+
+namespace nostalgia::studio {
+
+class Editor: public QWidget {
+	Q_OBJECT
+
+	public:
+		Editor(QWidget *parent);
+
+		virtual ~Editor() = default;
+
+		/**
+		 * Returns the name of item being edited.
+		 */
+		virtual QString itemName() = 0;
+
+		/**
+		 * Save changes to item being edited.
+		 */
+		virtual void save();
+
+		/**
+		 * Returns the undo stack holding changes to the item being edited.
+		 */
+		virtual QUndoStack *undoStack();
+
+};
+
+}

12c5339295644c71052baaa28d589f1fd2717b0e
Gary Talent <gtalent2@gmail.com>
2020-04-08 03:55:52 -0500

    [nostalgia] Update JsonCpp in conanfile

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,11 @@ from conans import ConanFile, CMake
 
 class NostalgiaConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "jsoncpp/1.9.0@theirix/stable", "sdl2/2.0.10@bincrafters/stable"#, "qt/5.14.0@bincrafters/stable" # comma-separated list of requirements
+    requires = "jsoncpp/1.9.2", "sdl2/2.0.10@bincrafters/stable", "qt/5.14.1@bincrafters/stable" # comma-separated list of requirements
     generators = "cmake", "cmake_find_package", "cmake_paths"
+    #default_options = {
+    #    "sdl2:nas": False
+    #}
 
     def requirements(self):
         pass

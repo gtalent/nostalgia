9560ccf476a887c20b2373eca52f38f186b6ed58
Gary Talent <gtalent2@gmail.com>
2020-05-05 00:03:58 -0500

    [nostalgia] Remove conan Qt, as it is currently being ignored

--- conanfile.py
+++ conanfile.py
@@ -2,11 +2,8 @@ from conans import ConanFile, CMake
 
 class NostalgiaConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "jsoncpp/1.9.2", "sdl2/2.0.10@bincrafters/stable", "qt/5.14.1@bincrafters/stable", "sqlite3/3.31.0", "libiconv/1.16"
+    requires = "jsoncpp/1.9.2", "sdl2/2.0.10@bincrafters/stable"
     generators = "cmake", "cmake_find_package", "cmake_paths"
     #default_options = {
     #    "sdl2:nas": False
     #}
-
-    def requirements(self):
-        pass

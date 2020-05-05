e7ea76a7acbf06995d97c9edd30f0c0eb0612e11
Gary Talent <gtalent2@gmail.com>
2020-05-04 19:25:10 -0500

    [nostalgia] Explicitly list certain dependencies that were causing problems on Fedora

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class NostalgiaConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "jsoncpp/1.9.2", "sdl2/2.0.10@bincrafters/stable", "qt/5.14.1@bincrafters/stable" # comma-separated list of requirements
+    requires = "jsoncpp/1.9.2", "sdl2/2.0.10@bincrafters/stable", "qt/5.14.1@bincrafters/stable", "sqlite3/3.31.0", "libiconv/1.16"
     generators = "cmake", "cmake_find_package", "cmake_paths"
     #default_options = {
     #    "sdl2:nas": False

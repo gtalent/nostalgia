e91e9ad58f4325535cfdd18848da06d92e7f5e69
Gary Talent <gary@drinkingtea.net>
2020-06-29 11:30:19 -0500

    [nostalgia/core] Cleanup

--- src/nostalgia/core/consts.hpp
+++ src/nostalgia/core/consts.hpp
@@ -1,11 +1,18 @@
+/*
+ * Copyright 2016 - 2020 gtalent2@gmail.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 #pragma once
 
 namespace nostalgia::core {
 
-constexpr auto PixelsPerTile = 64;
 constexpr auto TileWidth = 8;
 constexpr auto TileHeight = 8;
+constexpr auto PixelsPerTile = TileWidth * TileHeight;
 
 constexpr auto FileExt_ng = ".ng";
 constexpr auto FileExt_npal = ".npal";

5cd7f0cecf8e4c048df1df584f363a880679addf
Gary Talent <gary@drinkingtea.net>
2020-09-05 19:29:41 -0500

    [QDark] Make an Object library

---
+++ deps/QDark/include/qdark/theme.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2020 gary@drinkingtea.net
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
+namespace qdark {
+
+void load(QApplication *app);
+
+}

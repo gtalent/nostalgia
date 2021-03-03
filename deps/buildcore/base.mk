cb0645860a1421159bb44b610bc648ea4deedc15
Gary Talent <gary@drinkingtea.net>
2021-03-02 21:35:27 -0600

    [buildcore] Add copyright headers and license

--- deps/buildcore/base.mk
+++ deps/buildcore/base.mk
@@ -1,3 +1,11 @@
+#
+#  Copyright 2016 - 2021 gary@drinkingtea.net
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 ifeq (${OS},Windows_NT)
 	SHELL := powershell.exe
 	.SHELLFLAGS := -NoProfile -Command

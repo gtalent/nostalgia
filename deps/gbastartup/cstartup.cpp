763955a9bbd983400831f37715f7d88381430dc4
Gary Talent <gary@drinkingtea.net>
2021-03-15 22:03:35 -0500

    [gbastartup] Cleanup, make an object lib, and make dependency public

--- deps/gbastartup/cstartup.cpp
+++ deps/gbastartup/cstartup.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 - 2020 gary@drinkingtea.net
+ * Copyright 2016 - 2021 gary@drinkingtea.net
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,21 +9,13 @@
 #include <ox/std/bit.hpp>
 #include <ox/std/heapmgr.hpp>
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
+#define MEM_EWRAM_BEGIN ox::bit_cast<char*>(0x02000000)
+#define MEM_EWRAM_END   ox::bit_cast<char*>(0x0203FFFF)
 
-// this warning is too dumb to realize that it can actually confirm the hard
-// coded address aligns with the requirement of HeapSegment, so it must be
-// suppressed
-#pragma GCC diagnostic ignored "-Wcast-align"
-
-#define MEM_EWRAM_BEGIN reinterpret_cast<uint8_t*>(0x02000000)
-#define MEM_EWRAM_END   reinterpret_cast<uint8_t*>(0x0203FFFF)
-
-#define HEAP_BEGIN reinterpret_cast<ox::heapmgr::HeapSegment*>(MEM_EWRAM_BEGIN)
+#define HEAP_BEGIN ox::bit_cast<char*>(MEM_EWRAM_BEGIN)
 // set size to half of EWRAM
 #define HEAP_SIZE ((MEM_EWRAM_END - MEM_EWRAM_BEGIN) / 2)
-#define HEAP_END  reinterpret_cast<ox::heapmgr::HeapSegment*>(MEM_EWRAM_BEGIN + HEAP_SIZE)
+#define HEAP_END  ox::bit_cast<char*>(MEM_EWRAM_BEGIN + HEAP_SIZE)
 
 extern void (*__preinit_array_start[]) (void);
 extern void (*__preinit_array_end[]) (void);
@@ -47,10 +39,11 @@ void __libc_init_array() {
 
 int c_start() {
 	const char *args[2] = {"", "rom.oxfs"};
-	ox::heapmgr::initHeap(ox::bit_cast<char*>(HEAP_BEGIN), ox::bit_cast<char*>(HEAP_END));
+	ox::heapmgr::initHeap(HEAP_BEGIN, HEAP_END);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
 	return main(2, args);
-}
-
-}
-
 #pragma GCC diagnostic pop
+}
+
+}

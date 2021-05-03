ceebc05ee9bdec89f706f588ec294e91b51e6b7a
Gary Talent <gary@drinkingtea.net>
2021-05-03 14:34:17 -0400

    [ox/std] Add Buffer as Vector<char> alias

---
+++ deps/ox/src/ox/std/buffer.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2015 - 2021 gary@drinkingtea.net
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "error.hpp"
+#include "buffer.hpp"
+
+namespace ox {
+
+template class Vector<char>;
+
+}
